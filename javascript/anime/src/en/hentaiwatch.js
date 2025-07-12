b6369dbade6ce40281a2ffe2862fa1b604a656c0
hubgit120 <64881332+hubgit120@users.noreply.github.com>
2025-07-12 17:38:01 +0100

    Update hentaiwatch.js

--- javascript/anime/src/en/hentaiwatch.js
+++ javascript/anime/src/en/hentaiwatch.js
@@ -10,7 +10,7 @@ const mangayomiSources = [{
   "version": "0.0.1",
   "dateFormat": "",
   "dateFormatLocale": "",
-  "pkgPath": "anime/src/en/hentaiwatch.js"
+  "pkgPath": "javascript/anime/src/en/hentaiwatch.js"
 }];
 
 class DefaultExtension extends MProvider {
