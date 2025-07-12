22e5e7f279603bb7310e782b27baf2f66c342348
hubgit120 <64881332+hubgit120@users.noreply.github.com>
2025-07-12 17:56:28 +0100

    Update hanime1.js

--- javascript/anime/src/zh/hanime1.js
+++ javascript/anime/src/zh/hanime1.js
@@ -10,7 +10,7 @@ const mangayomiSources = [{
   "version": "0.0.1",
   "dateFormat": "",
   "dateFormatLocale": "",
-  "pkgPath": "javascript/anime/src/zh/hanime1.js"
+  "pkgPath": "anime/src/zh/hanime1.js"
 }];
 
 class DefaultExtension extends MProvider {
